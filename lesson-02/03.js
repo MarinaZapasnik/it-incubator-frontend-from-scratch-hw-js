348ba443b9a1e9231686961c4c5a3b677837894f
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-11-29 22:59:50 +0300

    commit

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -4,6 +4,24 @@
  * Результат сохраните в переменной grade.
  */
 
-const score = 70 // тестовое значение, можно изменять
-let grade
+const score = 70;
+let grade;
+
+if (score >= 0 && score < 50) {
+    grade = 'F'
+} else if (score >= 50 && score < 70) {
+    grade = 'D'
+} else if (score >= 70 && score < 80) {
+    grade = 'C'
+} else if (score >= 80 && score < 90) {
+    grade = 'B'
+} else if (score >= 90 && score <= 100) {
+    grade = 'A'
+}
+
+
+
+
+// тестовое значение, можно изменять
+
 // your code
