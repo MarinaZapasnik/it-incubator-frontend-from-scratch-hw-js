dea59b7ea7c2e260c9ca707a90fa7487f1939925
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2024-12-21 00:22:43 +0300

    commit

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,34 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+
+  //----------------------- перебор строки через цикл for
+
+  // let result = ''
+  // for (let i = 0; i < str.length; i ++ ) {
+  //   if (str[i-1] === undefined || str[i-1] === ' ') {
+  //     result = result + str[i].toUpperCase()
+  //   } else {
+  //     result = result + str[i]
+  //   }
+  // }
+  // return result
+
+  //------------------------- split for slice join
+
+  let result = str.split(' ')
+  for (let i = 0; i < result.length; i ++) {
+    result[i] = result[i][0].toUpperCase() + result[i].slice(1)
+  }
+  return result.join(' ')
+
+  //---------------------- использую split map slice и join 
+
+  // let result = str.split(' ')
+  // return result.map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+
+}
+
+console.log(capitalizeWords("hello world from javascript"));
+ 
